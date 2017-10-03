db31d9a796b296c499bfbef3fb3d0ffbcac0f831
saurav1009 <31587490+saurav1009@users.noreply.github.com>
2017-10-03 19:00:13 +0530

    added 3 files
    1. first provide the data set where the algo detects the face. -> dataset.py
    2. then, train the machine with the given dataset . -> trainer.py
    3. finally the machine detects the faces . -> detector1.py

---
+++ detector1.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
+
+cam=cv2.VideoCapture(0);
+rec=cv2.createLBPHFaceRecognizer();
+rec.load("recognizer\\trainingData.yml")
+id=0
+font=cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX,2,2,0,2,2)
+while(True):
+    ret,img=cam.read();
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces=faceDetect.detectMultiScale(gray,1.3,5);
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        id,conf=rec.predict(gray[y:y+h,x:x+w])
+        if(id==172443):
+            id="Saurav"
+        elif(id==172458):
+            id="Suravi"
+        cv2.cv.PutText(cv2.cv.fromarray(img),str(id),(x,y+h),font,255);
+    cv2.imshow("intro",img);
+    if(cv2.waitKey(1)==ord('q')):
+        break;
+cam.release()
+cv2.destroyAllWindows()
+    
